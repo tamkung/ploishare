aa0a5c393fd5025c5f21eba6a2dcef4faf13b0c4
Wichakool <88084670+Wichakool@users.noreply.github.com>
2023-01-09 10:53:49 +0700

    T

--- src/components/admin/IndexAdmin.js
+++ src/components/admin/IndexAdmin.js
@@ -9,18 +9,18 @@ import '../../css/Disable.css';
 export default function IndexAdmin() {
   return (
     <div>
-      <Header />
-      <MenuSideBar />
-      <div className='content-wrapper'>
-        <h1>Home</h1>
-      </div>
-      <Footer />
+
+
       <div className='d-none d-sm-block'>
         <Header />
         <MenuSideBar />
-        <ContentCar />
+        <div className='content-wrapper'>
+          <h1>Home</h1>
+        </div>
         <Footer />
       </div>
+
+
       <div className='container d-lg-none'>
         <div className='disable-nonti'>ระบบ Admin ไม่รองรับอุปกรณ์มือถือ</div>
       </div>
