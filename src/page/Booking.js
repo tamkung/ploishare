e49c8a88840bd5132c210306e64548db02c700bd
Wichakool <88084670+Wichakool@users.noreply.github.com>
2023-01-26 11:21:52 +0700

    T

--- src/page/Booking.js
+++ src/page/Booking.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { Carousel } from 'antd';
 import { Link, useParams } from 'react-router-dom'
 import axios from 'axios';
 import { API_URL } from "../Constant";
 import NO_Img from '../img/no_img.jpg';
 import '../css/Booking.css';
+
+
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
+
+
 function Booking() {
     const [getCar, setGetCar] = useState([]);
     const [Loading, setLoading] = useState(false);
@@ -29,7 +41,15 @@ function Booking() {
             {Loading !== false ?
                 <div className="flexbook">
                     <div className="itemBook">
-                        <img src={getCar.image !== null ? getCar.image : NO_Img} alt={getCar.license} />
+                        {/* <img src={getCar.image !== null ? getCar.image : NO_Img} alt={getCar.license} /> */}
+                        <Carousel autoplay>
+                            <div>
+                                <img src={getCar.image !== null ? getCar.image : NO_Img} alt={getCar.license} />
+                            </div>
+                            <div>
+                                <img src={getCar.image !== null ? getCar.image : NO_Img} alt={getCar.license} />
+                            </div>
+                        </Carousel>
                     </div>
                     <div className="itemBook" >
                         <div>
@@ -38,9 +58,9 @@ function Booking() {
                         </div>
                         <ul className="contentCar">
                             <li>สีรถ : {getCar.color}</li>
-                            <li>Car Seat : {getCar.seat}</li>
+                            <li>จำนวน : {getCar.seat} ที่นั่ง</li>
                         </ul>
-                        <button className="buttonNext-Outline" style={{ textAlign: "center", width: "70%", marginLeft: "15%" }}> จองเลย </button>
+                        <button className="button-book" style={{ textAlign: "center" }}> จองเลย </button>
                     </div>
                     <div className="detailCar">Car Detail : {getCar.detail}</div>
 
