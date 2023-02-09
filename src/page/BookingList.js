035119df838047e003962feeca370c4ff3a08978
Tamkung <hm82529@gmail.com>
2023-02-09 10:29:08 +0700

    loading commit data...

--- src/page/BookingList.js
+++ src/page/BookingList.js
@@ -6,7 +6,7 @@ import Card from 'react-bootstrap/Card';
 import '../css/Booking.css';
 import authCheck from '../service/Auth';
 import Swal from 'sweetalert2';
-import { Image, Modal } from 'antd';
+import { Image, Modal, Spin } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 import { GoAlert, GoEye } from "react-icons/go";
 import { BiImageAdd } from "react-icons/bi";
@@ -33,6 +33,7 @@ function BookingList() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [visible, setVisible] = useState(false);
   const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     originData.length = 0;
@@ -67,6 +68,7 @@ function BookingList() {
     });
     console.log('originData', originData);
     setGetBooking(originData);
+    setLoading(false);
     console.log('getBooking', getBooking);
   }
   const [componentDisabled, setComponentDisabled] = useState(false);
@@ -187,6 +189,7 @@ function BookingList() {
         confirmButtonText: 'Submit',
         showLoaderOnConfirm: true,
         preConfirm: (image) => {
+          setLoading(true);
           const formData = new FormData();
           console.log('image', image);
           formData.append('img', image);
@@ -202,6 +205,7 @@ function BookingList() {
               image: response.data,
             }).then((response) => {
               console.log(response.data);
+              setLoading(false);
               Swal.fire({
                 icon: 'success',
                 title: 'อัพโหลดรูปภาพสำเร็จ',
@@ -250,98 +254,100 @@ function BookingList() {
         </Modal>
         <hr className="my-4 ml-4 mr-4" style={{ border: "solid 1px white" }} />
       </div>
-      <div className='flexbox pl-2 pr-2 mt-3'>
-        {getBooking.map((item, index) => {
-          return (
-            <div className="item-list" key={index}>
-              <Card className='min-card'>
-                <Card.Header style={{ textAlign: "center" }}>สถานะปัจจุบัน  : {
-                  item.status === "0" ? "รออนุมัติ" :
-                    item.status === "1" ? "อนุมัติ" :
-                      item.status === "2" ? "กำลังใช้งาน" : "เสร็จสิ้น"}
-                </Card.Header>
-                <Card.Body style={{ minHeight: "160px" }}>
-                  <Card.Title className='flex'>
-                    ป้ายทะเบียน : {item.cLicense} {item.cName}
+      {loading === true ? <div className='flex justify-center items-center '><Spin /></div>
+        : <div className='flexbox pl-2 pr-2 mt-3'>
+          {getBooking.map((item, index) => {
+            return (
+              <div className="item-list" key={index}>
+                <Card className='min-card'>
+                  <Card.Header style={{ textAlign: "center" }}>สถานะปัจจุบัน  : {
+                    item.status === "0" ? "รออนุมัติ" :
+                      item.status === "1" ? "อนุมัติ" :
+                        item.status === "2" ? "กำลังใช้งาน" : "เสร็จสิ้น"}
+                  </Card.Header>
+                  <Card.Body style={{ minHeight: "160px" }}>
+                    <Card.Title className='flex'>
+                      ป้ายทะเบียน : {item.cLicense} {item.cName}
 
-                  </Card.Title>
-                  <Card.Text>
-                    ใช้งานวันที่ : {item.startDateTime}<br />
-                    <div className='flex items-center'>
-                      รูปอนุมัติ : {item.image ?
-                        <div className='flex items-center'>
-                          <GoEye className='text-primary ml-2 cursor-pointer'
-                            onClick={() => previewImage(true, item.image)} />
-                          <Image
-                            width={200}
-                            style={{ display: 'none' }}
-                            src={item.image}
-                            alt="รูปอนุมัติ"
-                            preview={{
-                              visible,
-                              src: image,
-                              onVisibleChange: (value) => {
-                                console.log(item.image);
-                                setVisible(value);
-                              },
-                            }}
-                          />
-                        </div>
-                        : <div className='flex items-center text-danger ml-2 '>
-                          ไม่มีรูปภาพ
-                          {/* <GoAlert className='text-danger' /> */}
-                          <BiImageAdd className='text-success cursor-pointer ml-2'
-                            size={20}
-                            onClick={() => uploadImage(item.id)}
-                          />
-                        </div>}
+                    </Card.Title>
+                    <Card.Text>
+                      ใช้งานวันที่ : {item.startDateTime}<br />
+                      <div className='flex items-center'>
+                        รูปอนุมัติ : {item.image ?
+                          <div className='flex items-center'>
+                            <GoEye className='text-primary ml-2 cursor-pointer'
+                              onClick={() => previewImage(true, item.image)} />
+                            <Image
+                              width={200}
+                              style={{ display: 'none' }}
+                              src={item.image}
+                              alt="รูปอนุมัติ"
+                              preview={{
+                                visible,
+                                src: image,
+                                onVisibleChange: (value) => {
+                                  console.log(item.image);
+                                  setVisible(value);
+                                },
+                              }}
+                            />
+                          </div>
+                          : <div className='flex items-center text-danger ml-2 '>
+                            ไม่มีรูปภาพ
+                            {/* <GoAlert className='text-danger' /> */}
+                            <BiImageAdd className='text-success cursor-pointer ml-2'
+                              size={20}
+                              onClick={() => uploadImage(item.id)}
+                            />
+                          </div>}
+                      </div>
+                    </Card.Text>
+                    {item.status === "2" ? <Card.Text>
+                      ไมล์เริ่มต้น : {item.startMile}</Card.Text> :
+                      item.status === "3" ? <Card.Text>
+                        ไมล์เริ่มต้น : {item.startMile}<br />
+                        ไมล์สิ้นสุด : {item.endMile} <br />
+                        ระยะทางที่ใช้ : {item.distance} กม.
+                      </Card.Text> : null}
+                  </Card.Body>
+                  <Card.Footer className="text-muted"><div className='text-center'>
+                    <div className={
+                      item.status === "0" ? "btn wait-cardlist" :
+                        item.status === "1" ? "btn turn-on-cardlist" :
+                          item.status === "2" ? "btn turn-off-cardlist" : "btn success-cardlist"}
+                      onClick={
+                        item.status === "0" ? () => { alert('รออนุมัติ') } :
+                          item.status === "1" ? () => { updateStatus(item.id, 2) } :
+                            item.status === "2" ? () => { updateStatus(item.id, 3) } : () => { alert('สิ้นสุดการใช้งาน') }}
+                    >
+                      {item.status === "0" ? "รออนุมัติ" :
+                        item.status === "1" ? "เปิดใช้งาน" :
+                          item.status === "2" ? "ปิดใช้งาน" : "สิ้นสุดการใช้งาน"}
                     </div>
-                  </Card.Text>
-                  {item.status === "2" ? <Card.Text>
-                    ไมล์เริ่มต้น : {item.startMile}</Card.Text> :
-                    item.status === "3" ? <Card.Text>
-                      ไมล์เริ่มต้น : {item.startMile}<br />
-                      ไมล์สิ้นสุด : {item.endMile} <br />
-                      ระยะทางที่ใช้ : {item.distance} กม.
-                    </Card.Text> : null}
-                </Card.Body>
-                <Card.Footer className="text-muted"><div className='text-center'>
-                  <div className={
-                    item.status === "0" ? "btn wait-cardlist" :
-                      item.status === "1" ? "btn turn-on-cardlist" :
-                        item.status === "2" ? "btn turn-off-cardlist" : "btn success-cardlist"}
-                    onClick={
-                      item.status === "0" ? () => { alert('รออนุมัติ') } :
-                        item.status === "1" ? () => { updateStatus(item.id, 2) } :
-                          item.status === "2" ? () => { updateStatus(item.id, 3) } : () => { alert('สิ้นสุดการใช้งาน') }}
-                  >
-                    {item.status === "0" ? "รออนุมัติ" :
-                      item.status === "1" ? "เปิดใช้งาน" :
-                        item.status === "2" ? "ปิดใช้งาน" : "สิ้นสุดการใช้งาน"}
-                  </div>
-                </div></Card.Footer>
-                <Card.Footer className="text-muted">จำนวนวัน : {item.day} วัน</Card.Footer>
-              </Card>
-            </div>
-            // <div className='card text-center'>
-            //   <p>เลขที่การจอง : {item.id}</p>
-            //   <p>จังหวัด : {item.province}</p>
-            //   <p>ชื่อ : {item.uName}</p>
-            //   <p>เลขที่พนักงาน : {item.empoyeeNo}</p>
-            //   <p>อีเมล : {item.uEmail}</p>
-            //   <p>เบอร์โทร : {item.uPhone}</p>
-            //   <p>แผนก : {item.uSect}</p>
-            //   <p>ฝ่าย : {item.uPart}</p>
-            //   <p>หมายเหตุ : {item.note}</p>
-            //   <p>วันที่เริ่ม : {item.startDateTime}</p>
-            //   <p>วันที่สิ้นสุด : {item.endDateTime}</p>
-            //   <p>ป้ายทะเบียน : {item.cLicense}</p>
-            //   <p>จำนวนวัน : {item.day}</p>
-            //   <p>สถานะ : {item.status}</p>
-            // </div>
-          )
-        })}
-      </div>
+                  </div></Card.Footer>
+                  <Card.Footer className="text-muted">จำนวนวัน : {item.day} วัน</Card.Footer>
+                </Card>
+              </div>
+              // <div className='card text-center'>
+              //   <p>เลขที่การจอง : {item.id}</p>
+              //   <p>จังหวัด : {item.province}</p>
+              //   <p>ชื่อ : {item.uName}</p>
+              //   <p>เลขที่พนักงาน : {item.empoyeeNo}</p>
+              //   <p>อีเมล : {item.uEmail}</p>
+              //   <p>เบอร์โทร : {item.uPhone}</p>
+              //   <p>แผนก : {item.uSect}</p>
+              //   <p>ฝ่าย : {item.uPart}</p>
+              //   <p>หมายเหตุ : {item.note}</p>
+              //   <p>วันที่เริ่ม : {item.startDateTime}</p>
+              //   <p>วันที่สิ้นสุด : {item.endDateTime}</p>
+              //   <p>ป้ายทะเบียน : {item.cLicense}</p>
+              //   <p>จำนวนวัน : {item.day}</p>
+              //   <p>สถานะ : {item.status}</p>
+              // </div>
+            )
+          })}
+        </div>
+      }
     </div>
   )
 }
