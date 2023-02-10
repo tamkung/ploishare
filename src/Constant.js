e7fd93c89a9ca596dcafc420c7f6c5a7e098598e
Tamkung <hm82529@gmail.com>
2023-02-10 23:02:31 +0700

    update

--- src/Constant.js
+++ src/Constant.js
@@ -1,10 +1,10 @@
 // export const API_URL = 'http://localhost:8080/'
 // export const API_URL_SignUp = 'http://localhost:8080/'
 
-// export const API_URL = 'https://api-ploishare.cyclic.app/'
-// export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
+export const API_URL = 'https://api-ploishare.cyclic.app/'
+export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
 
-export const API_URL = 'http://twtserver.ddns.net:3030/'
-export const API_URL_SignUp = 'http://twtserver.ddns.net:3030/'
+// export const API_URL = 'http://twtserver.ddns.net:3030/'
+// export const API_URL_SignUp = 'http://twtserver.ddns.net:3030/'
 
 export const GET_USER = JSON.parse(localStorage.getItem("user"));
