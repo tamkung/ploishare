e8015811fa830e6b6f8089c9cb77c98b677a30cf
Tamkung <hm82529@gmail.com>
2023-02-10 22:09:22 +0700

    3030

--- src/Constant.js
+++ src/Constant.js
@@ -4,7 +4,7 @@
 // export const API_URL = 'https://api-ploishare.cyclic.app/'
 // export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
 
-export const API_URL = 'http://twtserver.ddns.net:8080/'
-export const API_URL_SignUp = 'http://twtserver.ddns.net:8080/'
+export const API_URL = 'http://twtserver.ddns.net:3030/'
+export const API_URL_SignUp = 'http://twtserver.ddns.net:3030/'
 
 export const GET_USER = JSON.parse(localStorage.getItem("user"));
