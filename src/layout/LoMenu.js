b9c0d5512c3914b01b3670bda93f4fa60e73b814
Tamkung <hm82529@gmail.com>
2023-02-11 14:26:11 +0700

    menu

--- src/layout/LoMenu.js
+++ src/layout/LoMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { IonContent, IonFab, IonFabButton, IonFabList, IonIcon } from '@ionic/react';
-import { chevronUpCircle, list, logOut, home } from 'ionicons/icons';
+import { menu, list, logOut, home } from 'ionicons/icons';
 import { Link } from 'react-router-dom';
 import '../css/Booking.css';
 import SignOut from '../service/SignOut';
@@ -11,7 +11,7 @@ const MenuButton = () => (
     <IonContent className="ion-padding" color="secondary">
       <IonFab slot="fixed" horizontal="end" >
         <IonFabButton>
-          <IonIcon icon={chevronUpCircle}></IonIcon>
+          <IonIcon icon={menu}></IonIcon>
         </IonFabButton>
         <IonFabList side="top">
           <IonFabButton
