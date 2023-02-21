e89f5b51b8dba5cb30a2dc21a1da83ccb047fd75
Tamkung <hm82529@gmail.com>
2023-02-21 12:44:03 +0700

    preview

--- src/Constant.js
+++ src/Constant.js
@@ -1,8 +1,11 @@
 // export const API_URL = 'http://localhost:8080/'
 // export const API_URL_SignUp = 'http://localhost:8080/'
 
-export const API_URL = 'https://api-ploishare.cyclic.app/'
-export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
+// export const API_URL = 'https://api-ploishare.cyclic.app/'
+// export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
+
+export const API_URL = 'https://ploishare-api-preview.onrender.com/'
+export const API_URL_SignUp = 'https://ploishare-api-preview.onrender.com/'
 
 // export const API_URL = 'http://twtserver.ddns.net:3030/'
 // export const API_URL_SignUp = 'http://twtserver.ddns.net:3030/'
