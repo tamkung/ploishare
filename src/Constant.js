a3c7ab2e0285efa8e0438ff854b44f9bed819523
Tamkung <hm82529@gmail.com>
2023-05-12 08:41:31 +0700

    fix

--- src/Constant.js
+++ src/Constant.js
@@ -2,7 +2,8 @@
 // export const API_URL_SignUp = 'http://localhost:8080/'
 
 export const API_URL = 'https://api-ploishare.cyclic.app/'
-export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
+//export const API_URL_SignUp = 'https://test-w8q8.onrender.com/'
+export const API_URL_SignUp = 'https://api-ploishare.cyclic.app/'
 
 // export const API_URL = 'http://18.139.38.227:8080/'
 // export const API_URL_SignUp = 'http://18.139.38.227:8080/'
